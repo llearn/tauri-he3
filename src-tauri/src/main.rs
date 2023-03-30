7f44157b40217e571def7c788511943a089d7ec8
ccm <changming.chu@chiefclouds.com>
2023-03-30 17:55:22 +0800

    feat: 版本修改

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
+    let context = tauri::generate_context!();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+        .run(context)
         .expect("error while running tauri application");
 }
